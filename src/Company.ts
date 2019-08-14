72b636b21188dd110d73506b50a382d632b3e829
pwilkosz <piotr.wilkosz@rst.com.pl>
2019-08-14 07:53:55 +0200

    refactor

--- src/Company.ts
+++ src/Company.ts
@@ -2,6 +2,7 @@
 export class Company {
     public constructor(
         public legalName: string,
+        public locality: string,
         public lat: number,
         public lon: number,
     ) {}
